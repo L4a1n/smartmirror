dca3466d7f54bb40d9c43e54457d1332bb44b53e
L4a1n <150847641+L4a1n@users.noreply.github.com>
2024-06-27 10:10:06 +0200

    .

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import gi
+import asyncio
+from weather import getWeatherToday, getWeatherWeek
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
@@ -10,13 +13,15 @@ class MyWindow(Gtk.Window):
         self.set_border_width(10)
         self.set_default_size(400, 300)
         
-        button = Gtk.Button(label="Click Me")
+        button = Gtk.Button(label="get the weather")
         button.connect("clicked", self.on_button_clicked)
         
         self.add(button)
         
     def on_button_clicked(self, widget):
         print("Button was clicked!")
+        print(asyncio.run(getWeatherToday('Kassel')))
+        print(asyncio.run(getWeatherWeek('Kassel')))
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
--- weather.py
+++ weather.py
@@ -1,33 +1,21 @@
 # import the module
 import python_weather
-
 import asyncio
-import os
 
-async def getweather():
-  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
-  async with python_weather.Client() as client:
-    # fetch a weather forecast from a city
-    weather = await client.get('Kassel')
-    
-    # returns the current day's forecast temperature (int)
-    print(weather.temperature)
-    print(weather.feels_like)
-    print(weather.location)
-    print(weather.local_population)
-    print(weather.kind)
-    # get the weather forecast for a few days
-    #for daily in weather.daily_forecasts:
-    #  print(daily)
-      
-      # hourly forecasts
-      #for hourly in daily.hourly_forecasts:
-      #  print(f' --> {hourly!r}')
+# Return todays weather
+async def getWeatherToday(city):
+    async with python_weather.Client() as client:
+        try:
+            weather = await client.get(city)
+        except:
+            return "Something went wrong"
+    return weather.locale
 
-if __name__ == '__main__':
-  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-  # for more details
-  #if os.name == 'nt':
-  #  asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-   
-  asyncio.run(getweather())
+# Return this weeks weather
+async def getWeatherWeek(city):
+    async with python_weather.Client() as client:
+        try:
+            weather = await client.get(city)
+        except:
+            return "Something went wrong"
+    return weather.feels_like
