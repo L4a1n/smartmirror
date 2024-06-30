db44a080a8687c0de95a613b5d8a369d51055395
L4a1n <150847641+L4a1n@users.noreply.github.com>
2024-06-30 17:26:14 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
-# import the module
 import python_weather
 import asyncio
+from datetime import datetime
+import re
 
 # Return todays weather
 async def getWeatherToday(city):
@@ -11,23 +12,27 @@ async def getWeatherToday(city):
             weather = await client.get(city)
         except:
             return "Something went wrong"
+    toRemove = ["<", ">", "datetime.date", "datetime.time", "Kind."]
     # get the weather forecast for three days
+    # and format the output into usable strings for later 
+    tempDaily.append(f"lastRefreshed=({datetime.now().strftime("%H, %M, %S")})")
     for daily in weather.daily_forecasts:
-        tempDaily.append(daily)
-
-        # hourly forecasts for three days
+        temp = str(daily)
+        pattern = "|".join(re.escape(seq) for seq in toRemove)
+        temp = re.sub(pattern, "", temp)
+        tempDaily.append(temp)
+        tempHouhrly = []
         for hourly in daily.hourly_forecasts:
-            tempHouhrly.append(hourly)
+            temp = str(hourly)
+            pattern = "|".join(re.escape(seq) for seq in toRemove)
+            temp = re.sub(pattern, "", temp)
+            tempHouhrly.append(temp)
+        tempDaily.append(tempHouhrly)
+
     print(tempDaily)
+    print(len(tempDaily))
+    return tempDaily
     
 
-# Return this weeks weather
-async def getWeatherWeek(city):
-    async with python_weather.Client() as client:
-        try:
-            weather = await client.get(city)
-        except:
-            return "Something went wrong"
-    return weather.feels_like
 
 asyncio.run(getWeatherToday('Kassel'))
